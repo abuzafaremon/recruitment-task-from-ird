878d6e46b184a2350d236e8244c28170b934dec8
Abu Zafar Emon <zafar emon>
2023-11-07 22:11:05 +0600

    header component complete

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import Image from "next/image";
+import { BsNintendoSwitch, BsSearch, BsSunFill } from "react-icons/bs";
+import { HiMenu } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container mx-auto bg-white"> header </div>
+    // header start
+    <header className="py-2.5 md:py-3 px-4 md:px-6">
+      <div className="container mx-auto bg-white">
+        <div className="flex justify-between items-center">
+          {/* header left start */}
+          <div className="w-16 md:w-[70px] h-8 md:h-9">
+            <Image
+              className="w-full"
+              width={70}
+              height={36}
+              src="/assets/images/logo/header-logo.png"
+              alt="logo"
+            />
+          </div>
+          {/* header left end */}
+          {/* header middle start */}
+          <div className="w-[390px] hidden md:block">
+            <div className="border-2 border-[#F0F2F4] rounded-lg p-1 flex justify-between items-center">
+              <input
+                className="border-none outline-none px-2.5 placeholder:text-[#101010] placeholder:text-xs"
+                type="text"
+                placeholder="Search Best Food"
+              />
+              <div className="bg-[#FCEEE9] border border-[#FADFD6] rounded-md ring-1 ring-[#FCEEE9] p-2">
+                <BsSearch className="text-[#e55527] text-[13px]" />
+              </div>
+            </div>
+          </div>
+          {/* header middle end */}
+          {/* header last start */}
+          <div className="flex items-center gap-6">
+            <div className="bg-[#FCEEE9] border border-[#FADFD6] rounded-md ring-1 ring-[#FCEEE9] p-2 md:hidden">
+              <BsSearch className="text-[#e55527] text-[13px]" />
+            </div>
+            <div className="bg-[#FCEEE9] border border-[#FADFD6] rounded-md ring-1 ring-[#FCEEE9] p-2 hidden md:block">
+              <BsSunFill className="text-[#e55527] text-[13px]" />
+            </div>
+            <div className="bg-[#FCEEE9] border border-[#FADFD6] rounded-md ring-1 ring-[#FCEEE9] p-2 hidden md:block">
+              <BsNintendoSwitch className="text-[#101010] text-[13px]" />
+            </div>
+            <div className="bg-[#E7E7E7] rounded-md p-2">
+              <HiMenu className="text-[#101010] text-[13px]" />
+            </div>
+          </div>
+          {/* header last end */}
+        </div>
+      </div>
     </header>
+    // header end
   );
 };
 
