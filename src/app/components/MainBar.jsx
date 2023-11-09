e8fc8d0a1d9e08a180e4437d109baa4c02c6ea9e
Abu Zafar Emon <zafar emon>
2023-11-09 20:05:26 +0600

    fruits list completed

--- src/app/components/MainBar.jsx
+++ src/app/components/MainBar.jsx
@@ -1,8 +1,60 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { BsSearch } from "react-icons/bs";
 
 const MainBar = () => {
+  // create mock data for mapping
+  const fruitsList = [
+    {
+      id: 1,
+      name: "oranges",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/orange.png",
+    },
+    {
+      id: 2,
+      name: "apple",
+      vitamin: "vitamin b6",
+      img: "/assets/images/fruits/apple.png",
+    },
+    {
+      id: 3,
+      name: "avocado",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/avocado.png",
+    },
+    {
+      id: 4,
+      name: "avocado",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/avocado.png",
+    },
+    {
+      id: 5,
+      name: "avocado",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/avocado.png",
+    },
+    {
+      id: 6,
+      name: "avocado",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/avocado.png",
+    },
+    {
+      id: 7,
+      name: "avocado",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/avocado.png",
+    },
+    {
+      id: 8,
+      name: "avocado",
+      vitamin: "vitamin c",
+      img: "/assets/images/fruits/avocado.png",
+    },
+  ];
   return (
     <div className="bg-white p-4 rounded-xl w-full">
       <div className="flex flex-col gap-[15px]">
@@ -34,6 +86,37 @@ const MainBar = () => {
           </div>
         </div>
         {/* search box end */}
+
+        {/* items div start  */}
+        <div>
+          <h3 className="text-xs font-medium text-[#101010] mb-2.5">
+            Fruits List
+          </h3>
+          {/* map fruitsLists */}
+          <div className="flex flex-col gap-1.5">
+            {fruitsList.map((fruit) => (
+              // single fruit div
+              <div
+                key={fruit.id}
+                className="flex items-center gap-4 px-2.5 py-2 border-2 border-transparent rounded-[10px] hover:border-[#E55527]"
+              >
+                <div className="w-11 h-11 md:w-14 md:h-14">
+                  <Image
+                    src={fruit.img}
+                    alt={fruit.name}
+                    width={56}
+                    height={56}
+                  />
+                </div>
+                <div className="capitalize text-[#101010] flex flex-col gap-2">
+                  <h3 className="text-sm font-medium">{fruit.name}</h3>
+                  <p className="text-xs">{fruit.vitamin}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* items div end  */}
       </div>
     </div>
   );
